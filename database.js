f934d7f7e06fdd86087a5bc9388a6fd6aca7441b
Daniel Pineda Torres <dpineda.dev@gmail.com>
2024-11-08 16:51:31 -0600

    Added a better mysql client to work with mysql-server, since testing was previously done in xampp

--- database.js
+++ database.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql');
-const util = require('util');
+const mysql = require("mysql2");
+const util = require("util");
 
 const pool = mysql.createPool({
-    connectionLimit:10,
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'nodejs'
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "170304",
+  database: "nodejs",
 });
 
 pool.query = util.promisify(pool.query);
